bda0f392edc078a9aa9e6db680996eca22c3d3d8
Wes Weber <wweber@wesweber.com>
2013-07-06 17:04:58 -0400

    Add feature uninstalling

--- gui/FeatureUninstaller.cpp
+++ gui/FeatureUninstaller.cpp
@@ -6,5 +6,59 @@
  */
 
 #include "winfeatures.h"
+#include "winservices.h"
+#include "libdebloat/ServiceController.h"
+#include "libdebloat/PackageManager.h"
+#include <list>
+#include <string>
+#include <cstring>
 
+void uninstallFeatures(const std::list<const char*>& pFuncs)
+{
+	PackageManager pkg;
+	int features = sizeof(gFeatures) / sizeof(Feature);
+	for (std::list<const char*>::const_iterator itr = pFuncs.begin();
+			itr != pFuncs.end(); ++itr)
+	{
+		for (int i = 0; i < features; ++i)
+		{
+			if (strcmp(*itr, gFeatures[i].function) == 0)
+			{
+				//uninstall
+				try
+				{
+					pkg.setFeatureEnabled(gFeatures[i].name, false);
+				} catch (const std::exception&)
+				{
 
+				}
+			}
+		}
+	}
+}
+
+void disableServices(const std::list<const char*> pFuncs)
+{
+	ServiceController svc;
+	int services = sizeof(gServices) / sizeof(Service);
+
+	for (std::list<const char*>::const_iterator itr = pFuncs.begin();
+			itr != pFuncs.end(); ++itr)
+	{
+		for (int i = 0; i < services; ++i)
+		{
+			if (strcmp(*itr, gServices[i].function) == 0)
+			{
+				//disable
+				try
+				{
+					svc.changeServiceType(gServices[i].name,
+							ServiceController::DISABLED);
+				} catch (const std::exception&)
+				{
+
+				}
+			}
+		}
+	}
+}
--- gui/ui/Menu.cpp
+++ gui/ui/Menu.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "ui/Menu.h"
-#include <cstdio>
+#include "FeatureUninstaller.h"
 
 Menu::Menu()
 {
@@ -20,5 +20,54 @@ Menu::~Menu()
 
 void Menu::apply()
 {
-	printf("Apply button pressed.\n");
+	//disable all controls (i think)
+	setEnabled(false);
+
+	//TODO: thread this out
+
+	//build list of functionalities to remove
+
+	std::list<const char*> list;
+
+	if (mUi.disable_xps->isChecked())
+		list.push_back("xps");
+	if (mUi.disable_gadgets->isChecked())
+		list.push_back("gadgets");
+	if (mUi.disable_tablet->isChecked())
+		list.push_back("tablet");
+	if (mUi.disable_dotNet->isChecked())
+		list.push_back("dotnet");
+	if (mUi.disable_search->isChecked())
+		list.push_back("search");
+	if (mUi.disable_games->isChecked())
+		list.push_back("games");
+	if (mUi.disable_printing->isChecked())
+		list.push_back("printing");
+	if (mUi.disable_ie->isChecked())
+		list.push_back("ie");
+	if (mUi.disable_media->isChecked())
+		list.push_back("media");
+	//if (mUi.disable_homegroup->isChecked())
+	//	list.push_back("homegroup");
+	//if (mUi.disable_sharing->isChecked())
+	//	list.push_back("sharing");
+	//if (mUi.disable_fax->isChecked())
+	//	list.push_back("fax");
+	//if (mUi.disable_remote->isChecked())
+	//	list.push_back("remote");
+	//if (mUi.disable_bluetooth->isChecked())
+	//	list.push_back("bluetooth");
+
+	//add services if requested
+	if (mUi.disable_services->isChecked())
+		list.push_back("none");
+
+	//remove features
+	uninstallFeatures(list);
+
+	//disable services
+	disableServices(list);
+
+	//now close
+	close();
 }
