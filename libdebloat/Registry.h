35d9debaea17735a8bf87ece1ada3aee0a6940e1
Wes Weber <wweber@wesweber.com>
2013-07-07 12:19:29 -0400

    Document Registry functions

--- libdebloat/Registry.h
+++ libdebloat/Registry.h
@@ -12,13 +12,47 @@
 #include <vector>
 #include <string>
 
+/**
+ * Get a value from the registry
+ * @param pRoot The root key
+ * @param pKey The name of the key to query
+ * @param pValue The name of the value to query
+ * @param pData Pointer to where the data will be stored
+ * @param pSize The size available in pData
+ */
 void getRegistryKeyValue(HKEY pRoot, const char* pKey, const char* pValue,
 		void* pData, int* pSize);
+
+/**
+ * Sets a registry key value
+ * @param pRoot The root key
+ * @param pKey The name of the subkey
+ * @param pValue The name of the value to set
+ * @param pType The type of the value to set. See Windows defines.
+ * @param pData Pointer to the data to copy
+ * @param pSize The size of the data to copy
+ */
 void setRegistryKeyValue(HKEY pRoot, const char* pKey, const char* pValue,
 		int pType, const void* pData, int pSize);
 
-std::vector<std::string> getRegistryValues(HKEY pRoot, const char* pKey, int pMisc = 0);
+/**
+ * Get a list of registry values in a key.
+ * @param pRoot The root key
+ * @param pKey The name of the subkey
+ * @param pMisc Additional flags. Mainly used for registry redirection
+ * @return
+ */
+std::vector<std::string> getRegistryValues(HKEY pRoot, const char* pKey,
+		int pMisc = 0);
 
-void deleteRegistryValue(HKEY pRoot, const char* pKey, const char* pValue, int pMisc = 0);
+/**
+ * Delete a registry value
+ * @param pRoot The root key
+ * @param pKey The key to operate on
+ * @param pValue The name of the value to delete
+ * @param pMisc Additional flags. Mainly used for registry redirection
+ */
+void deleteRegistryValue(HKEY pRoot, const char* pKey, const char* pValue,
+		int pMisc = 0);
 
 #endif /* REGISTRY_H_ */
